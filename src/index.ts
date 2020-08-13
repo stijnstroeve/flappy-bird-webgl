b454babcfd2d341400f8c260afdd6e64d3c5242c
Stijn Stroeve <stijnstroeve@hotmail.com>
2020-08-13 20:33:34 +0200

    Added webpack. Added index.hmtl.

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+console.log('test2');
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const canvas: HTMLCanvasElement = <HTMLCanvasElement> document.getElementById('game');
+    const gl: WebGLRenderingContext = canvas.getContext('webgl');
+
+});
